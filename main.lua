dfbb9c70d3aa6c394d4ae2e3d4a7089cd217e60c
haidm39 <haidm@gearinc.com>
2020-04-14 10:14:39 +0700

    Finish breakout 0

---
+++ main.lua
@@ -0,0 +1,105 @@
+-- Managing depencies
+require 'src/Dependencies'
+
+function love.load()
+    --init filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    --seed random
+    math.randomseed(os.time())
+    --set title
+    love.window.setTitle('Breakout')
+    --init fonts
+    gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+    }
+    love.graphics.setFont(gFonts['small'])
+    --init sounds
+    gSounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+        ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+        ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+        ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+        ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+        ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+        ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+        ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+
+        ['music'] = love.audio.newSource('sounds/music.wav', 'static')
+    }
+    --init textures
+    gTextures = {
+        ['background'] = love.graphics.newImage('graphics/background.png'),
+        ['main'] = love.graphics.newImage('graphics/breakout.png'),
+        ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+        ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+        ['particle'] = love.graphics.newImage('graphics/particle.png')
+    }
+    --init screen
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullScreen = false,
+        resizable = true
+    })
+    --init state machine
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end
+    }
+    gStateMachine:change('start')
+    --init keyboards table
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(width, height)
+    push:resize(width, height)
+end
+
+function love.update(dt) 
+    gStateMachine:update(dt)
+    -- reset keyspressed
+    love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.draw()
+    --begin drawing with push, in our virtual resolution
+    push:apply('start')
+
+    local backgroundWidth = gTextures['background']:getWidth()
+    local backgroundHeight = gTextures['background']:getHeight()
+
+    love.graphics.draw(gTextures['background'],
+                        0, 0,
+                        0,
+                        VIRTUAL_WIDTH / (backgroundWidth - 1), VIRTUAL_HEIGHT / (backgroundHeight - 1)
+    )
+
+    gStateMachine:render()
+
+    displayFPS()
+
+    push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.setColor(0/255, 255/255, 0/255, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 5, 5)
+end
